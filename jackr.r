6ca23500f654c727b6af9c06319279b278db3796
Jack Leff <leffjr@appstate.edu>
2016-02-01 20:15:22 -0500

    fly free birds

---
+++ jackr.r
@@ -0,0 +1,8 @@
+#' @description Question and answer
+#' @details explains the functions in the
+#' @docType package
+#' @name leffjr - jackr
+#' @title jackr help me
+#' @section Stuff: Stuffed Crust.
+#' @keywords package jackr testing
+NULL
